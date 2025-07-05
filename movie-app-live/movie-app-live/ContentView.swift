deb8778674fc7cc927779dd8e15a00df41a4bd0b
Gergo Szabo <szabohedergergo@gmail.com>
2025-07-05 17:28:35 +0200

    Creating project

---
+++ movie-app-live/movie-app-live/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movie-app-live
+//
+//  Created by Gergo Szabo on 2025. 07. 05..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movie-app-live/movie-app-live/movie_app_liveApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_app_liveApp.swift
+//  movie-app-live
+//
+//  Created by Gergo Szabo on 2025. 07. 05..
+//
+
+import SwiftUI
+
+@main
+struct movie_app_liveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
