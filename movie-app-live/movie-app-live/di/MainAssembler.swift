26f5fa74033c661c4f6cd152bac62baf6dc20177
Gergo Szabo <szabohedergergo@gmail.com>
2025-07-06 18:17:57 +0200

    all

---
+++ movie-app-live/movie-app-live/di/MainAssembler.swift
@@ -0,0 +1,39 @@
+//
+//  MainAssembler.swift
+//  movie-app-live
+//
+//  Created by Gergo Szabo on 2025. 07. 05..
+//
+
+import Swinject
+
+class MainAssembler {
+    public static var instance: MainAssembler! != nil
+    
+    var resolver: Resolver {
+        return assembler.resolver
+    }
+    
+    let container: Container
+    private let assembler: Assembler
+    
+    private init(withAssemblies assemblies: [Assembly]) {
+        container = Container()
+        assembler = Assembler(container: container)
+        assembler.apply(assemblies: assemblies)
+    }
+    
+    static func create(withAssemblies assemblies: [Assembly]) -> MainAssembler {
+        instance = MainAssembler(withAssemblies: assemblies)
+        return instance
+        
+    }
+    
+    deinit{
+        log.debug("deinit")
+    }
+    
+    func dispose(){
+        log.debug("dispose")
+    }
+}
--- movie-app-live/movie-app-live/di/ViewModelAssembly.swift
+++ movie-app-live/movie-app-live/di/ViewModelAssembly.swift
@@ -5,3 +5,10 @@
 //  Created by Gergo Szabo on 2025. 07. 05..
 //
 
+import Swinject
+import Moya
+import Foundation
+
+class ViewModelAssembly: Assembly { //viewmodell függőségek
+    
+}
